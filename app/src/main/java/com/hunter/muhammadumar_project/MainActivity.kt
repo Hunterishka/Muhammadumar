23144073d7c27e5f238a25e1b8f45a5cedcf30cf
Muhammadrasul06 <muhammadrasul9909@gmail.com>
2022-12-02 20:08:48 +0500

    Third commit

--- app/src/main/java/com/hunter/muhammadumar_project/MainActivity.kt
+++ app/src/main/java/com/hunter/muhammadumar_project/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.hunter.muhammadumar_project
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.hunter.muhammadumar_project.databinding.ActivityMainBinding
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
     }
 }
